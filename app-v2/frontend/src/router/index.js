3c27c711c82745ac5db72c7975610fa2b3692bf9
lidia chikhoune <stratimaagency@gmail.com>
2026-04-17 11:23:03 +0000

    fix: properly add /cgu and /privacy routes to router

--- app-v2/frontend/src/router/index.js
+++ app-v2/frontend/src/router/index.js
@@ -48,7 +48,9 @@ const routes = [
       { path: 'resources/guides', name: 'resources-guides', component: () => import('@/views/resources/GuidesView.vue') },
       { path: 'resources/tools', name: 'resources-tools', component: () => import('@/views/resources/ToolsView.vue') },
       { path: 'resources/wellbeing', name: 'resources-wellbeing', component: () => import('@/views/resources/WellbeingResourcesView.vue') },
-      { path: '/:pathMatch(.*)*', name: 'NotFound', component: () => import('@/views/NotFoundView.vue') },
+        { path: '/cgu', component: () => import('@/views/legal/CguView.vue') },
+  { path: '/privacy', component: () => import('@/views/legal/PrivacyView.vue') },
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: () => import('@/views/NotFoundView.vue') },
 ],
   },
 ]
